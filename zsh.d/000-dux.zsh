08b406985c800a1ff0591d0788defc5a3bfb3620
Felix Wong <felix.wong@okta.com>
2019-07-08 15:07:59 -0700

    add option to disable dux

--- zsh.d/000-dux.zsh
+++ zsh.d/000-dux.zsh
@@ -1,10 +1,14 @@
-if [[ ! $TERM =~ screen  ]]; then
-    ~/bin/dux || echo "Failed to run Dux (returned with $?)"
-    echo -n "Exiting in 3 "
-    sleep 1
-    echo -n "2 "
-    sleep 1
-    echo -n "1 "
-    sleep 1
-    exit
+if [[ -z "$DISABLE_DUX" ]]
+then
+  if [[ ! "$TERM" =~ screen  ]]
+  then
+      ~/bin/dux || echo "Failed to run Dux (returned with $?)"
+      echo -n "Exiting in 3 "
+      sleep 1
+      echo -n "2 "
+      sleep 1
+      echo -n "1 "
+      sleep 1
+      exit
+  fi
 fi
