9bfabdf8146cc9e5f5645069020b8c69b1c6483e
Felix Wong <waf@waf.hk>
2020-12-24 18:11:30 -0800

    fixes for darwin

--- zsh.d/000-dux.zsh
+++ zsh.d/000-dux.zsh
@@ -1,14 +1,17 @@
 if [[ -z "$DISABLE_DUX" ]]
 then
-  if [[ ! "$TERM" =~ screen  ]] && [[ ! "$(tty)" =~ "tty" ]]
+  if [[ ! "$TERM" =~ screen  ]]
   then
-      ~/bin/dux || echo "Failed to run Dux (returned with $?)"
-      echo -n "Exiting in 3 "
-      sleep 1
-      echo -n "2 "
-      sleep 1
-      echo -n "1 "
-      sleep 1
-      exit
+    if [[ $(uname -s) == "Darwin" ]] || [[ ! "$(tty)" =~ "tty" ]]
+    then
+        ~/bin/dux || echo "Failed to run Dux (returned with $?)"
+        echo -n "Exiting in 3 "
+        sleep 1
+        echo -n "2 "
+        sleep 1
+        echo -n "1 "
+        sleep 1
+        exit
+    fi
   fi
 fi
