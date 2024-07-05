005f6b57f6426d40593d295a22b10f2c51a00927
Felix Wong <felix@fawong.com>
2024-07-05 00:41:13 +0000

    ignore root

--- dotfiles/zsh.pre.d/0-dux.zsh
+++ dotfiles/zsh.pre.d/0-dux.zsh
@@ -1,19 +1,22 @@
 if [[ -z "$DISABLE_DUX" ]]
 then
-  if [[ ! "$(tty)" =~ "tty" ]] || [[ "$(uname -s)" == "Darwin" ]]
+  if [[ "$UID" -ne 0 ]]
   then
-    if [[ -z "$TMUX" ]]
+    if [[ ! "$(tty)" =~ "tty" ]] || [[ "$(uname -s)" == "Darwin" ]]
     then
-        ~/bin/dux || echo "Failed to run Dux (returned with $?)"
+      if [[ -z "$TMUX" ]]
+      then
+          ~/bin/dux || echo "Failed to run Dux (returned with $?)"
 
-        echo -n "Exiting in 3 "
-        sleep 1
-        echo -n "2 "
-        sleep 1
-        echo -n "1 "
-        sleep 1
+          echo -n "Exiting in 3 "
+          sleep 1
+          echo -n "2 "
+          sleep 1
+          echo -n "1 "
+          sleep 1
 
-        exit 0
+          exit 0
+      fi
     fi
   fi
 fi
