09fd9ff0e2f74f6001cd32cab080925fed742d67
Felix Wong <felix@siftstack.com>
2026-07-15 14:11:52 -0700

    treesitter cli

--- dotfiles/config/nvim/lua/plug_conf/treesitter.lua
+++ dotfiles/config/nvim/lua/plug_conf/treesitter.lua
@@ -1,31 +1,42 @@
 local DISABLE_HIGHLIGHT_THRESHOLD = 30000
 
-local configs = require("nvim-treesitter.configs")
+local ts = require("nvim-treesitter")
 
-configs.setup({
-  ensure_installed = {
-    "bash",
-    "c",
-    "cpp",
-    "css",
-    "go",
-    "html",
-    "javascript",
-    "lua",
-    "python",
-    "ruby",
-    "rust",
-    "sql",
-    "typescript",
-    "yaml",
-  },
-  auto_install = true,
-  sync_install = false,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-    disable = function(_lang, _bufnr)
-      return vim.api.nvim_buf_line_count(0) > DISABLE_HIGHLIGHT_THRESHOLD
-    end,
-  }
+local ensure_installed = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "go",
+  "html",
+  "javascript",
+  "lua",
+  "python",
+  "ruby",
+  "rust",
+  "sql",
+  "typescript",
+  "yaml",
+}
+
+ts.install(ensure_installed)
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("TreesitterHighlight", {}),
+  callback = function(args)
+    local buf = args.buf
+    if vim.api.nvim_buf_line_count(buf) > DISABLE_HIGHLIGHT_THRESHOLD then
+      return
+    end
+    local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
+    if not lang or not vim.tbl_contains(ts.get_available(), lang) then
+      return
+    end
+    -- auto-install missing parsers, then attach (no-op install if present)
+    ts.install(lang):await(function()
+      if vim.api.nvim_buf_is_valid(buf) then
+        pcall(vim.treesitter.start, buf, lang)
+      end
+    end)
+  end,
 })
